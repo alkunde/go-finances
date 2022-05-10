d6fe8edeb709af00c75dad775d7aa798a103bcca
André Kunde <andre.kunde@zup.com.br>
2022-05-10 08:35:48 -0300

    transaction buttons

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import { Button } from '../../components/Form/Button';
+import { Input } from '../../components/Form/Input';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionType,
+} from './styles';
+
+export function Register() {
+  const [transactionType, setTransactionType] = useState('');
+
+  function handleTransactionTypeSelected(type: 'up' | 'down') {
+    setTransactionType(type);
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input
+            placeholder='Nome'
+          />
+          <Input
+            placeholder='Preço'
+          />
+          <TransactionType>
+            <TransactionTypeButton
+              title='Income'
+              type='up'
+              onPress={() => handleTransactionTypeSelected('up')}
+              isActive={transactionType === 'up'}
+            />
+            <TransactionTypeButton
+              title='Outcome'
+              type='down'
+              onPress={() => handleTransactionTypeSelected('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionType>
+        </Fields>
+
+        <Button title='Enviar' />
+      </Form>
+    </Container>
+  )
+}
