6801c86aee2af0791412354dd7852d3f757bcdef
André Kunde <andre.kunde@zup.com.br>
2022-05-10 08:54:20 -0300

    category component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
 
 import {
   Container,
@@ -48,6 +49,8 @@ export function Register() {
               isActive={transactionType === 'down'}
             />
           </TransactionType>
+
+          <CategorySelect title='Categoria' />
         </Fields>
 
         <Button title='Enviar' />
